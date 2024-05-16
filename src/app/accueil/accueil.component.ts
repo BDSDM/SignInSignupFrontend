e3d4ffadd3e571035c22597544eeaccca199612f
Destin <bdsdmiii@yahoo.fr>
2024-05-16 12:45:34 +0200

    Add Display Delete action

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,15 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AccueilService } from './accueil.service';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {
-  constructor( private router: Router) { }
+export class AccueilComponent implements OnInit {
+  users: User[] = [];
+
+  constructor(private router: Router, private accueilService: AccueilService) { }
+
+  ngOnInit(): void {
+    this.displayUsers();
+  }
+
   logOut() {
     this.router.navigate(['/login']);
   }
 
+  displayUsers() {
+    this.accueilService.getAllUsers().subscribe(
+      data => {
+        this.users = data;
+      },
+      error => {
+        console.error('There was an error!', error);
+      }
+    );
+  }
+  deleteUser(id: number) {
+    this.accueilService.deleteUser(id).subscribe(
+      () => {
+        // Supprimer l'utilisateur localement après une suppression réussie
+        this.users = this.users.filter(user => user.id !== id);
+      },
+      error => {
+        console.error('There was an error deleting the user!', error);
+      }
+    );
+  }
 }
---
+++ src/app/accueil/user.model.ts
@@ -0,0 +1,8 @@
+// src/app/models/user.model.ts
+export interface User {
+    id: number;
+    username: string;
+    password?: string; // Optionnel, selon vos besoins
+    // Ajoutez d'autres champs nécessaires
+  }
+  
