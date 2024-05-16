e89d68acb7b70a236e5c5cfdb450d496bebd91f9
Destin <bdsdmiii@yahoo.fr>
2024-05-16 18:18:26 +0200

    Secure the routes

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccueilService } from './accueil.service';
 import { User } from './user.model';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-accueil',
@@ -11,13 +12,14 @@ import { User } from './user.model';
 export class AccueilComponent implements OnInit {
   users: User[] = [];
 
-  constructor(private router: Router, private accueilService: AccueilService) { }
+  constructor(private authService: AuthService,private router: Router, private accueilService: AccueilService) { }
 
   ngOnInit(): void {
     this.displayUsers();
   }
 
   logOut() {
+    this.authService.logout();
     this.router.navigate(['/login']);
   }
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'acceuil', component: AccueilComponent },
+  { path: 'accueil', component: AccueilComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' }
  
 ];
 
